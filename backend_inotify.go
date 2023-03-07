f10c8f4a5519dff70ac3c962090ca9fd91066dac
徐焱 <xuyan4@staff.sina.com.cn>
2023-03-07 11:16:27 +0800

    fix bug

--- backend_inotify.go
+++ backend_inotify.go
@@ -360,7 +360,7 @@ func (w *Watcher) AddWith(name string, opts ...addOpt) error {
 	var flags uint32 = unix.IN_MOVED_TO | unix.IN_MOVED_FROM |
 		unix.IN_CREATE | unix.IN_ATTRIB | unix.IN_MODIFY |
 		unix.IN_MOVE_SELF | unix.IN_DELETE | unix.IN_DELETE_SELF |
-		unix.IN_CLOSE | IN_CLOSE_NOWRITE | IN_CLOSE_WRITE
+		unix.IN_CLOSE | unix.IN_CLOSE_NOWRITE | unix.IN_CLOSE_WRITE
 
 	return w.watches.updatePath(name, func(existing *watch) (*watch, error) {
 		if existing != nil {
